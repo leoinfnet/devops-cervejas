ff21acbba3ea0c4ee99709bdd69ee4eda3f01b25
leonardogloria <contato@leonardogloria.com.br>
2024-02-20 17:29:17 -0300

    Arquivos

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,8 @@
-FROM postgres
-COPY acme.sql /docker-entrypoint-initdb.d/
+FROM amazoncorretto:17.0.10
+WORKDIR /app
+ARG localizacao_build=/tmp/secret.properties
+ENV localizacao=$localizacao_build
+EXPOSE 8080
+COPY target/cervejas-0.0.1-SNAPSHOT.jar app.jar
+COPY secret-prod.properties /tmp/secret.properties
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/br/com/acme/cervejas/CervejaController.java
+++ src/main/java/br/com/acme/cervejas/CervejaController.java
@@ -1,18 +1,41 @@
 package br.com.acme.cervejas;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/")
 public class CervejaController {
+    @Value("${LOCATION}")
+    private String location;
     @Autowired
     CervejaRepository cervejaRepository;
     @GetMapping
     public ResponseEntity<?> getAll(){
         return ResponseEntity.ok(cervejaRepository.findAll());
     }
+    @GetMapping("/file")
+    public ResponseEntity<?> getFromFile(){
+
+        Path path = Paths.get(location);
+        List<String> strings = new ArrayList<>();
+        try {
+            strings = Files.readAllLines(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return ResponseEntity.ok(strings);
+    }
+
 }
